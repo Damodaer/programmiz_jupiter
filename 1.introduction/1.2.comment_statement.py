36bf1f64a1b5ffd3142f371d67f2b53fc851f32f
damodar <damodarsahuofficial94@gmail.com>
2022-08-09 20:55:22 +0530

    initiial commit

---
+++ 1.introduction/1.2.comment_statement.py
@@ -0,0 +1,53 @@
+# # multi line statement
+
+# num1 = 1+2+3+4+ \
+#        +5+6
+
+# print(num1)
+
+# num2 = (1+2+3+4+5+6)
+
+# print(num2)
+
+# colors = ['red',
+# 'green',
+# 'pink']
+
+# colors2 = [2+
+# 2+
+# 6]
+
+# print(colors2)
+
+
+
+
+# for i in range(1,11):
+    # print(ikdj)
+    # 
+    # 
+    # 
+def myName():
+    """commented code""" #docstring
+    print('ashok')
+
+print(myName.__doc__)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
